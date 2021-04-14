e484f78a41f6ea54dab74911920f80b1f317749b
gR <Gr>
2021-04-14 18:58:43 +0300

    cupcake corner

---
+++ Cupcake Corner/CheckoutView.swift
@@ -0,0 +1,82 @@
+//
+//  CheckoutView.swift
+//  Cupcake Corner
+//
+//  Created by admin on 13.04.2021.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    @ObservedObject var order: Order
+    @State private var confirmationMessage = ""
+    @State private var showingConfirmation = false
+    @State private var showingError = false
+    
+    var body: some View {
+        GeometryReader { geo in
+            ScrollView {
+                VStack {
+                    Image("cupcakes")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: geo.size.width)
+                    
+                    Text("Your total is $\(self.order.cost, specifier: "%.2f")")
+                        .font(.title)
+                    
+                    Button("Place order") {
+                        self.placeOrder()
+                    }
+                    .padding()
+                }
+            }
+        }
+        .navigationBarTitle("Check out", displayMode: .inline)
+        .alert(isPresented: $showingConfirmation, content: {
+            Alert(title: Text("Thank you"), message: Text(confirmationMessage), dismissButton: .default(Text("OK")))
+        })
+        .alert(isPresented: $showingError, content: {
+            Alert(title: Text("Opps"), message: Text("Some error"), dismissButton: .default(Text("OK(")))
+        })
+    }
+    
+    func placeOrder() {
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("failed to encode data")
+            return
+        }
+        
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        request.httpBody = encoded
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if error != nil {
+                self.showingError = true
+                return
+            }
+            
+            guard let data = data else {
+                print("No data in response")
+                return
+            }
+            
+            if let decodedOrder = try? JSONDecoder().decode(Order.self, from: data) {
+                self.confirmationMessage = "Your order for \(decodedOrder.quantity)x \(Order.types[decodedOrder.type].lowercased()) cupcakes is on its way!"
+                self.showingConfirmation = true
+            } else {
+                print("invalid response from server")
+            }
+            
+        }.resume()
+    }
+}
+
+struct CheckoutView_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckoutView(order: Order())
+    }
+}
--- Cupcake Corner/ContentView.swift
+++ Cupcake Corner/ContentView.swift
@@ -8,9 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+   @ObservedObject var order = Order()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    Picker("Select your cake type", selection: $order.type) {
+                        ForEach(0 ..< Order.types.count, id: \.self) {
+                            Text(Order.types[$0])
+                        }
+                    }
+                    
+                    Stepper(value: $order.quantity, in: 3...20) {
+                        Text("Number of cakes: \(order.quantity)")
+                    }
+                }
+                Section {
+                    Toggle(isOn: $order.specialRequestEnabled.animation(), label: {
+                        Text("Any special request")
+                    })
+                    
+                    if order.specialRequestEnabled {
+                        Toggle(isOn: $order.extraFrosting, label: {
+                            Text("Add extra frosting")
+                        })
+                        
+                        Toggle(isOn: $order.addSprinkles, label: {
+                            Text("Add sprinkles")
+                        })
+                    }
+                }
+                
+                Section {
+                    NavigationLink(
+                        destination: AddressView(order: order),
+                        label: {
+                            Text("Delivery details")
+                        })
+                }
+            }
+            .navigationBarTitle("Cupcake Corner")
+        }
     }
 }
 
